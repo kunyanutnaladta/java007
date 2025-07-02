34207327d6dc49c11cd5ebff8d4ff26dd8b96528
kunyanutnaladta <kittiya.pha@kkumail.com>
2025-07-02 14:01:52 +0700

    name claass
    dasdas

--- CircleAreaCalculator1.java
+++ CircleAreaCalculator1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CircleAreaCalculator {
+public class CircleAreaCalculator1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
